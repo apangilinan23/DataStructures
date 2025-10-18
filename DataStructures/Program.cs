b2987b9470ed1147ed64f74976a58b23a368e817
apangilinan <pangilinan_adrian21@yahoo.com>
2025-10-18 09:10:23 +0800

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,10 @@
+﻿var numbers = new List<int>();
+numbers.Add(1);//index 0
+numbers.Add(2);//index 1
+numbers.Add(3);//index 2
+
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}

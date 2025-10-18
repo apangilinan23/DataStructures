714d1de3547a66e740472bbd86dd443ed2ecdcdb
adrian <pangilinan.adrian21@gmail.com>
2025-10-18 13:38:19 +0800

    add loops for other datastructures

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,10 +1,34 @@
-﻿var numbers = new List<int>();
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+var numbers = new List<int>();
 numbers.Add(1);//index 0
 numbers.Add(2);//index 1
 numbers.Add(3);//index 2
 
-
+Console.WriteLine("List");
 foreach (var number in numbers)
 {
     Console.WriteLine(number);
+}
+
+var numbersArray = new int[3];
+numbersArray[0] = 1;
+numbersArray[1] = 2;
+numbersArray[2] = 3;
+
+Console.WriteLine("Array");
+foreach (var number in numbersArray)
+{
+    Console.WriteLine(number);
+}
+
+var numbersDictionary = new Dictionary<string,int>();
+numbersDictionary["one"] = 1;
+numbersDictionary["two"] = 2;
+numbersDictionary["three"] = 3;
+
+Console.WriteLine("Dictionary");
+foreach (var key in numbersDictionary.Keys)
+{
+    Console.WriteLine(numbersDictionary[key]);
 }
